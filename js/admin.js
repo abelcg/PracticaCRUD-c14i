0681d53507ca520097b22e12eaab44421184ee20
abelcg <abelcg86@gmail.com>
2022-12-05 21:31:37 -0300

    se agrego datos de prueba

--- js/admin.js
+++ js/admin.js
@@ -15,6 +15,7 @@ let campoCantidad = document.getElementById("cantidad");
 let campoUrl = document.getElementById("URL");
 let formularioProducto = document.querySelector("#formProduto");
 let btnNuevo = document.getElementById("btnNuevo");
+let btnDatosPrueba = document.getElementById("btnDatosPrueba");
 
 let productoExistente = false; //variable bandera: si es false quiere crear producto y si true quiero modicar Producto
 //Si hay productos en localstorage, quiero guardar en el array de productos y si no que sea un array vacio.
@@ -44,6 +45,7 @@ campoUrl.addEventListener("blur", () => {
 
 formularioProducto.addEventListener("submit", guardarProducto);
 btnNuevo.addEventListener("click", limpiarFormulario);
+btnDatosPrueba.addEventListener("click", cargarDatosPrueba);
 
 //invoco a cargaInicial: si tengo productos en el localStorage los mustra en la tabla.
 cargaInicial();
@@ -249,4 +251,74 @@ window.borrarProducto = function (codigo) {
       );
     }
   });
+};
+
+
+function cargarDatosPrueba(){
+  const datos = [
+    {
+      codigo: "994",
+      producto: "Kakashi Hatake (Anbu)",
+      cantidad: "1",
+      descripcion:
+        "Funko Figura Pop Naruto Shippuden Kakashi Hatake (Anbu) (AAA Anime Exclusive)",
+      url: "https://m.media-amazon.com/images/I/51Mkr80aQqL._AC_SL1092_.jpg",
+    },
+    {
+      codigo: "933",
+      producto: "Shikamaru Nara",
+      cantidad: "1",
+      descripcion: "Naruto shippuden",
+      url: "https://m.media-amazon.com/images/I/51BitznofnL._AC_SL1300_.jpg",
+    },
+    {
+      codigo: "184",
+      producto: "Tobi",
+      cantidad: "1",
+      descripcion:
+        "Figura de Tobi de Naruto Shippuden de la marca FunKo POP Anime",
+      url: "https://m.media-amazon.com/images/I/51-H7QOsVES._AC_SL1200_.jpg",
+    },
+    {
+      codigo: "729",
+      producto: "Orochimaru",
+      cantidad: "1",
+      descripcion: "Orochimaru Figura Coleccionable, Multicolor (46628)",
+      url: "https://m.media-amazon.com/images/I/610cunP4zOL._AC_SL1200_.jpg",
+    },
+    {
+      codigo: "073",
+      producto: "Jiraiya On Toad",
+      cantidad: "1",
+      descripcion:
+        "Jiraiya On Toad Anime Figura De Acción Juguetes 73 Colección Modelo De Personaje Estatua 10 Cm En Caja",
+      url: "https://m.media-amazon.com/images/I/61sLJuTZxBS._AC_SL1500_.jpg",
+    },
+    {
+      codigo: "728",
+      producto: "Gaara ",
+      cantidad: "1",
+      descripcion: "Gaara Figura Coleccionable, Multicolor (46627)",
+      url: "https://m.media-amazon.com/images/I/616YRHWRZwL._AC_SL1200_.jpg",
+    },
+    {
+      codigo: "182",
+      producto: "Kakashi Figure",
+      cantidad: "1",
+      descripcion:
+        'Funko FM-B01M5KD9Y6 Naruto Shippuden 12450"POP Vinyl Kakashi Figure',
+      url: "https://m.media-amazon.com/images/I/617XvrkXkEL._AC_SL1360_.jpg",
+    },
+  ];
+
+ if (!localStorage.getItem('arrayProductoKey')) {
+   // quiero agregar los datos de productos
+   console.log('cargar datos prueba');
+   localStorage.setItem('arrayProductoKey', JSON.stringify(datos));
+   listaProductos = datos;
+   //mostar en la tabla
+   listaProductos.forEach(itemProducto => {
+     crearFila(itemProducto);
+   })
+ }
 };
